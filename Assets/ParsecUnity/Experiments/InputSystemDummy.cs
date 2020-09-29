33ef078d54372a62615fe5408d8bcd2f06154e00
Erik <erikroystan@gmail.com>
2020-09-28 20:39:33 -0700

    Add InputSystem experiment.

---
+++ Assets/ParsecUnity/Experiments/InputSystemDummy.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.LowLevel;
+
+public class InputSystemDummy : MonoBehaviour
+{
+    [SerializeField]
+    int executeFrame = 1;
+
+    [SerializeField]
+    public bool doA;
+
+    [SerializeField]
+    public bool doB;
+
+    [Header("Debug")]
+    public bool pressedA;
+    public bool pressedB;
+
+    private Keyboard keyboard;
+
+    private void Start()
+    {
+        keyboard = InputSystem.AddDevice<Keyboard>();
+    }
+
+    private void Update()
+    {
+        if (Time.frameCount == executeFrame)
+        {
+            using (StateEvent.From(keyboard, out InputEventPtr eventPtr))
+            {
+                keyboard[Key.A].WriteValueIntoEvent<float>(1, eventPtr);
+                keyboard[Key.B].WriteValueIntoEvent<float>(1, eventPtr);
+                InputSystem.QueueEvent(eventPtr);
+            }
+
+            //if (doA)
+            //{
+            //    using (StateEvent.From(keyboard, out InputEventPtr eventPtr))
+            //    {
+            //        keyboard[Key.A].WriteValueIntoEvent<float>(1, eventPtr);
+            //        InputSystem.QueueEvent(eventPtr);
+            //    }
+            //}
+
+            //if (doB)
+            //{
+            //    using (StateEvent.From(keyboard, out InputEventPtr eventPtr))
+            //    {
+            //        keyboard[Key.B].WriteValueIntoEvent<float>(1, eventPtr);
+            //        InputSystem.QueueEvent(eventPtr);
+            //    }
+            //}
+        }
+        else if (Time.frameCount == executeFrame * 2)
+        {
+            using (StateEvent.From(keyboard, out InputEventPtr eventPtr))
+            {
+                keyboard[Key.A].WriteValueIntoEvent<float>(0, eventPtr);
+                keyboard[Key.B].WriteValueIntoEvent<float>(0, eventPtr);
+                InputSystem.QueueEvent(eventPtr);
+            }
+
+            //if (doA)
+            //{
+            //    using (StateEvent.From(keyboard, out InputEventPtr eventPtr))
+            //    {
+            //        keyboard[Key.A].WriteValueIntoEvent<float>(0, eventPtr);
+            //        InputSystem.QueueEvent(eventPtr);
+            //    }
+            //}
+
+            //if (doB)
+            //{
+            //    using (StateEvent.From(keyboard, out InputEventPtr eventPtr))
+            //    {
+            //        keyboard[Key.B].WriteValueIntoEvent<float>(0, eventPtr);
+            //        InputSystem.QueueEvent(eventPtr);
+            //    }
+            //}
+        }
+
+        pressedA = keyboard[Key.A].ReadValue() > 0.1f;
+        pressedB = keyboard[Key.B].ReadValue() > 0.1f;
+    }
+
+    private void OnDestroy()
+    {
+        InputSystem.RemoveDevice(keyboard);
+    }
+}
